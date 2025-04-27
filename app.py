fa9c53ba4cefc140aea8b98e59bf148558907aac
Danish Abbas Syed <danishabbas.syed@polimi.it>
2025-04-28 00:27:16 +0200

    updated app version2

--- app.py
+++ app.py
@@ -1,20 +1,61 @@
-from flask import Flask , render_template, request
+from flask import Flask , render_template, request, jsonify
 from ngsiOperations.ngsildOperations.ngsildEntityCreator import*
-from ngsiOperations.ngsildOperations.ngsildSensorProvision import*
 from ngsiOperations.ngsildOperations.ngsildCrudOperations import*
-#from ngsiOperations.ngsildOperations.ngsildSubscriptions import createSubscriptions
 from waitress import serve
+from orionClient.orion_client import *
 import threading
 import queue
 import os
+from flask_cors import CORS
+from datetime import datetime, timezone
+import json
+import requests
+import logging
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
+ORION_LD_URL = os.getenv("ORION_LD_URL", "localhost")
+ORION_LD_PORT = os.getenv("ORION_LD_PORT", "1026")
+CONTEXT_URL = os.getenv("CONTEXT_URL", "localhost")
+CONTEXT_PORT = os.getenv("CONTEXT_PORT", "5051")
+MADE_ORION_LD_URL = os.getenv("MADE_ORION_LD_URL", "localhost")
+AGV_URL = os.getenv("AGV_URL", "localhost")
+AGV_PORT = os.getenv("AGV_PORT", "5000")
 
-def index():
-    return render_template('index.html')
+def orderQuantity(order_list):
+    order_qty = 0
+    for order in order_list:
+        qty = order.get("orderQuantity")
+        if isinstance(qty, (int, float)):
+            order_qty += qty
+        else:
+            logger.warning(f"Order {order.get('id')} is missing 'requestQty' or it is not a number.")
+            continue
+    return order_qty
+
+def dispatch_agv(location):
+    paylod = json.dumps({"location": location})
+    url = f"http://{AGV_URL}:{AGV_PORT}/dispatch"
+    try:
+        response = requests.post(url ,data=paylod) # headers={"Content-Type": "application/json"
+        if response.status_code == 200:
+            logger.info("AGV dispatched successfully.")
+            return True
+        else:
+            logger.error(f"Failed to dispatch AGV: {response.status_code} - {response.text}")
+            return False
+    except requests.RequestException as e:
+        logger.error(f"Error dispatching AGV: {e}")
+        return False
+
+
+################Routes#######################
+@app.route('/')
+def home():
+    incomplete_orders, processing_orders, completed_orders = extract_entity_data(ORION_LD_URL, ORION_LD_PORT, CONTEXT_URL, CONTEXT_PORT, ENTITY_TYPE="Order")
+    return render_template("index.html", incomplete_orders=incomplete_orders, processing_orders=processing_orders, completed_orders=completed_orders)
 
 @app.route('/incomplete_orders', methods=['GET'])
 def display_incomplete_orders():
@@ -63,38 +104,64 @@ def display_complete_orders():
     # Render the HTML template with the incomplete orders data
     return render_template('complete_orders.html', orders=complete_orders)
 
-@app.route('/get_rawmaterials')
-def getrawmat():
-    print('this downloads the data of a trial')
-
-@app.route('/dispatch_order')
-def dispatchmaterial():
-    print('this downloads the data of a trial')
+@app.route("/start_production", methods=["POST"])
+def start_production():
+    data = request.get_json(silent=True) or {}
+    mode = data.get("mode", "Baseline")
+    n=4
+    incomplete_orders, _, _ = extract_entity_data(ORION_LD_URL, ORION_LD_PORT, CONTEXT_URL, CONTEXT_PORT, ENTITY_TYPE="Order")
+    print(f"Number of incomplete orders: {len(incomplete_orders)}")
+    if len(incomplete_orders) != 0:
+        for i in range(n):
+            if orderQuantity(incomplete_orders[:i+1]) <= n: 
+                print(f"Processing order {i+1} with quantity {orderQuantity(incomplete_orders[:i+1])}")
+                continue
+            else:
+                break
+        in_process_list = incomplete_orders[:i]
+        timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-2]
+        response_patch =update_processing_order_list(in_process_list, ORION_LD_URL, ORION_LD_PORT, CONTEXT_URL, CONTEXT_PORT,timestamp)
+        if response_patch:
+            response_agv = dispatch_agv(location=1) # check how to fix if agv is not dispatched
+            return jsonify({"success": True})
+        else:
+            return jsonify({"success": False})
+        # if response_patch:
+        #     return jsonify({"success": True})
+        # else:
+        #     return jsonify({"success": False})
+    else:
+        return jsonify({"Status": "No orders to process"})
     
-@app.route('/setup')
-def initial_setup():
-    orion= "localhost"
-    port=1026
-    context="context"
-    d1 ={
-    "id": "urn:ngsi-ld:order:order002",
-    "type": "order",
-        "incompleteOrderList": {
-            "type": "Property",
-            "value": [["1004","2024-01-16T17:50:07.5870Z",5],["1005","2024-01-16T17:51:07.5870Z",4]]
-        },
-        "processingOrderList": {
-            "type": "Property",
-            "value": []
-        },
-        "completedOrderList": {
-            "type": "Property",
-            "value": [["1001","2024-01-16T17:50:07.5870Z",4],["1002","2024-01-16T17:51:07.5870Z",1]]
-        }
-    }
-    r2 = ngsi_create_entity(d1,orion="localhost",orion_port=1026,context="context",context_port=5051)
-    print(r2.status_code)
-    return r2
+@app.route("/complete_production", methods=["POST"])
+def complete_production():
+    global lea_order
+    _, in_process_list, _ = extract_entity_data(ORION_LD_URL, ORION_LD_PORT, CONTEXT_URL, CONTEXT_PORT, ENTITY_TYPE="Order")
+    if in_process_list:
+        timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%fZ")[:-2]
+        response = update_complete_order_list(in_process_list, ORION_LD_URL, ORION_LD_PORT, CONTEXT_URL, CONTEXT_PORT,timestamp)
+        if response:
+            response_agv = dispatch_agv(location=0)
+            return jsonify({"success": True})
+        else:
+            return jsonify({"success": False})
+    else:
+        return jsonify({"Status": "No orders to complete"})
+
+@app.route('/get_orders', methods=['GET'])# Done
+def get_order():
+    incomplete_orders, processing_orders, comleted_orders = extract_entity_data(ORION_LD_URL, ORION_LD_PORT, CONTEXT_URL, CONTEXT_PORT, ENTITY_TYPE="Order")
+    return jsonify({"incomplete_orders": incomplete_orders, "processing_orders": processing_orders, "completed_orders": comleted_orders})
+
+@app.route('/get_completed_orders', methods=['GET']) # Done
+def get_order_info():
+    _, _, comleted_orders = extract_entity_data(ORION_LD_URL, ORION_LD_PORT, CONTEXT_URL, CONTEXT_PORT, ENTITY_TYPE="Order")
+    return jsonify({"completed_orders": comleted_orders})
+
+@app.route('/history', methods=['GET'])#
+def history():
+    _, _, comleted_orders = extract_entity_data(ORION_LD_URL, ORION_LD_PORT, CONTEXT_URL, CONTEXT_PORT, ENTITY_TYPE="Order")
+    return render_template("history.html", completed_orders=comleted_orders)
 
 if __name__ == "__main__":
     serve(app, host= "0.0.0.0", port= 3005)
---
+++ static/script_p1.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const startBtn = document.getElementById('start-batch');
+    const endBtn = document.getElementById('complete-batch');
+    startBtn.addEventListener('click', function () {
+        const mode = document.getElementById('mode').value;  // Get selected mode from dropdown
+        fetch('/start_production', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ mode: mode })  // Send mode in the request body
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                startBtn.disabled = true;
+                fetchOrders();
+            } else {
+                alert('Failed to start production:No orders available');
+            }
+        });
+    });
+
+    endBtn.addEventListener('click', function () {
+        fetch('/complete_production', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                endBtn.disabled = true;
+                fetchOrders();
+            } else {
+                alert('Failed to complete production:No orders available');
+            }
+        });
+    });
+    
+    function fetchOrders() {
+        fetch('/get_orders')
+        .then(response => response.json())
+        .then(data => {
+            // Update counts
+            document.getElementById('waiting').textContent = data.incomplete_orders.length;
+            document.getElementById('inprocess').textContent = data.processing_orders.length;
+            document.getElementById('processed').textContent = data.completed_orders.length;
+    
+            // Update Current Batch Table
+            const currentBatchBody = document.getElementById('current-batch');
+            currentBatchBody.innerHTML = '';
+            data.processing_orders.forEach(order => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${order.id.slice(24)}</td>
+                    <td>${order.productionStartTime}</td>
+                    <td>${order.orderQuantity}</td>
+                `;
+                currentBatchBody.appendChild(row);
+            });
+    
+            // Update Waiting List Table
+            const waitingListBody = document.getElementById('waiting-list');
+            waitingListBody.innerHTML = '';
+            data.incomplete_orders.forEach(order => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${order.id.slice(24)}</td>
+                    <td>${order.creationTime}</td>
+                    <td>${order.orderQuantity}</td>
+                `;
+                waitingListBody.appendChild(row);
+            });
+    
+            // Enable/Disable buttons based on order state
+            if (data.processing_orders.length > 0) {
+                startBtn.disabled = true;   
+                                    
+            } else {
+                startBtn.disabled = false;
+            }
+        });
+    }
+    
+
+    // Initial fetch and auto-update every 3 seconds
+    fetchOrders();
+    setInterval(fetchOrders, 5000);
+});
